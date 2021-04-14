f61d61f91217e1c67ccd1afc3b0a9b7d4206794f
ChiliPaneer <venkatratnamcp@gmail.com>
2021-04-14 18:17:58 -0400

    working on adding models

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,6 +4,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 
+from sqlalchemy import Table, MetaData, Column, Integer, String, TIMESTAMP, DATETIME
+from sqlalchemy import text
+from sqlalchemy.schema import FetchedValue
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:P@ssw0rd@localhost/python_test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'FALSE'
@@ -34,6 +38,21 @@ print('new feautre on demoBranch')
 
 if __name__ == '__main__':
     app.run()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(Integer, primary_key=True)
+    first_name = Column(String(45))
+    last_name = Column(String(45))
+    username = Column(String(45))
+    password = Column(String(45))
+    date_of_birth = Column(
+        DATETIME,
+        server_default=text("CURRENT_DATETIME ON UPDATE CURRENT_DATETIME"),
+        #server_onupdate=FetchedValue()
+    )
+
 
 
 
