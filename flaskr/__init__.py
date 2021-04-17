20e54e09aff57db072781cb699c81eda0bf2c11d
Christina Long <long.c@husky.neu.edu>
2021-04-17 15:08:51 -0400

    edited create_app config

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,22 +3,50 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
+import os
 
+db = SQLAlchemy()
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:P@ssw0rd@localhost/python_test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:C9Sneaky@localhost/python_test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'FALSE'
 
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-db = SQLAlchemy(app)
+# Create flask app
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    db.init_app(app)
+
+    with app.app_context():
+        from . import routes  # Import routes
+        db.create_all()  # Create sql tables for our data models
+
+        return app
+
+
 
 Base = automap_base()
-
 engine = create_engine(
-    'mysql+pymysql://root:P@ssw0rd@localhost/python_test',
+    'mysql+pymysql://root:C9Sneaky@localhost/python_test',
     echo=True
 )
 
