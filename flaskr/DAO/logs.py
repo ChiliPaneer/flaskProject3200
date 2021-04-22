7c9a960ddf2f413a67e9ea648aa0253638805ca5
ChiliPaneer <venkatratnamcp@gmail.com>
2021-04-22 15:52:44 -0400

    Completed generic Logs DAO, (untested)

--- flaskr/DAO/logs.py
+++ flaskr/DAO/logs.py
@@ -1,1 +1,26 @@
 #TODO REFACTOR AS DISCUSSED VENKAT
+from flaskr.models import db, Logs, Users, Aircrafts, CategoryEnum, CertificateEnum, Ratings
+
+def createLogs(params):
+    newLog = Logs(params)
+    db.session.add(newLog)
+    db.session.commit()
+
+def updateLogs():
+    return -1
+
+
+def findLogsById(id):
+    obj = db.session.query(Logs).get(id)
+    return obj
+
+
+def findAllLogs():
+    obj = db.session.query(Logs).all()
+    return obj
+
+def deleteLogs(id):
+    obj = db.session.query.get(id)
+    db.session.delete(obj)
+    db.session.commit()
+
